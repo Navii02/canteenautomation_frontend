4fc29180c2b6168c7afcbc36d7220429c8baeecf
Navii02 <naveenshaji02@gmail.com>
2025-03-24 22:59:34 +0530

    set

--- src/components/User/Profile.jsx
+++ src/components/User/Profile.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./profile.css";
+import "./Profile.css";
 import Dashboard from "./Dashboard";
 import { url } from "../../service/serviceurl";
 const Profile = () => {
